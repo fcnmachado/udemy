09d9fba343b9660cfff5692ae07940387952d847
Felipe Machado <fcnmachado@gmail.com>
2019-10-10 18:26:38 -0300

    Section 3 (11-12)

--- node/notes-app/app.js
+++ node/notes-app/app.js
@@ -1,8 +1,10 @@
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
 const msg = getNotes()
-
 console.log(msg)
+
+console.log(chalk.green.inverse.bold('Success!'))
 
 //const add = require('./utils.js')
 //const sum = add(4, -2)
