b0a2951438a3c2e19974bfcfe802742d9835f1a5
Felipe Machado <fcnmachado@gmail.com>
2019-10-18 20:44:37 -0300

    40. Destructuring and Property

--- node/weather-app/app.js
+++ node/weather-app/app.js
@@ -7,19 +7,19 @@ const address = process.argv[2]
 if (!address){
     console.log('Please provide an address')
 }else{
-    geocode.geocode(address, (error, data) => {
+    geocode.geocode(address, (error, {latitude, longitude, location}) => {
 
         if(error){
            return console.log(error)
         }
     
-        forecast.forecast(data.latitude, data.longitude, (error, forecastData) => {
+        forecast.forecast(latitude, longitude, (error, {summary, temperature, precipProbability}) => {
             if (error){
                 return console.log(error)
             }
     
-            console.log(data.location)
-            console.log(forecastData.summary + `. It's currently ${forecastData.temperature} degrees out. There is a ${forecastData.precipProbability}% chance of rain.`)
+            console.log(location)
+            console.log(summary + `. It's currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`)
         })
     })
     
--- node/weather-app/utils/forecast.js
+++ node/weather-app/utils/forecast.js
@@ -3,7 +3,7 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/c1e5531969975918bc326ab354eb6324/' + latitude + ',' + longitude + `?&units=si`
 
-    request({url: url, json: true}, (error, response) => {
+    request({url, json: true}, (error, response) => {
         if(error){
             callback('Unable to connect to weather service!')
         }else if (response.body.error){
