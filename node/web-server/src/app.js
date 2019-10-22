870a490c99f44f88d5b86ca0b04a982e3e1002e0
Felipe Machado <fcnmachado@gmail.com>
2019-10-22 20:25:24 -0300

    55. Building a JSON HTTP Endpoint

--- node/web-server/src/app.js
+++ node/web-server/src/app.js
@@ -2,6 +2,9 @@ const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
 
+const geocode = require('./utils/geocode.js')
+const forecast = require('./utils/forecast.js')
+
 const app = express()
 
 // Define paths for Express config
@@ -45,10 +48,23 @@ app.get('/weather', (req, res) => {
             error: 'You must provide an address term.'
         })
     }
-    res.send({
-        forecast: 'It is snowing',
-        location: 'Niteroi',
-        address: req.query.address
+    geocode.geocode(req.query.address, (error, {latitude, longitude, location}) => {
+        if(error){
+            return res.send({ error })
+        }
+        
+        forecast.forecast(latitude, longitude, (error, forecastData) => {
+            if(error){
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+
     })
 })
 
---
+++ node/web-server/src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/c1e5531969975918bc326ab354eb6324/' + latitude + ',' + longitude + `?&units=si`
+
+    request({url, json: true}, (error, response) => {
+        if(error){
+            callback('Unable to connect to weather service!')
+        }else if (response.body.error){
+            callback(`Unable to find location!`)
+        }else{
+            callback(error, response.body.currently)
+        }
+    })
+}
+
+module.exports = {
+    forecast : forecast
+}
