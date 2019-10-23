400f85b4329a1d5e1d6b06f14b92f540f160aae7
Felipe Machado <fcnmachado@gmail.com>
2019-10-22 21:11:42 -0300

    57. Browser HTTP Requests with

--- node/web-server/public/js/app.js
+++ node/web-server/public/js/app.js
@@ -1,1 +1,12 @@
 console.log('Java script app.js file loaded!')
+
+fetch('http://localhost:3000/weather?address=niteroi').then((response) => {
+    response.json().then((data) => {
+        if(data.error){
+            console.log(data.error)
+        }else{
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
