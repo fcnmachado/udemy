26b7094108ee37167db434af00080a0a84153bc0
Felipe Machado <fcnmachado@gmail.com>
2019-10-24 13:08:14 -0300

    59. Wiring up the User Interface

--- node/web-server/src/utils/forecast.js
+++ node/web-server/src/utils/forecast.js
@@ -3,13 +3,14 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/c1e5531969975918bc326ab354eb6324/' + latitude + ',' + longitude + `?&units=si`
 
-    request({url, json: true}, (error, response) => {
+    request({url, json: true}, (error, {body} = {}) => {
         if(error){
             callback('Unable to connect to weather service!')
-        }else if (response.body.error){
+        }else if (body.error){
             callback(`Unable to find location!`)
         }else{
-            callback(error, response.body.currently)
+            const forecast = `${body.currently.summary}. It's currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.` 
+            callback(error, forecast)
         }
     })
 }
